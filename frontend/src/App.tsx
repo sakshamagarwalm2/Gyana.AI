f0ab8b851dcb68e6fbdbe301daf32c2cb26ff92b
sakshamagarwalm2 <sakshamagarwalm2@gmail.com>
2024-12-07 13:35:57 +0000

    frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+
+function App() {
+
+  return (
+    <main className='flex justify-center items-center w-full h-screen bg-black'>
+      hello
+    </main>
+  )
+}
+
+export default App
---
+++ frontend/src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { createTheme, ThemeProvider } from '@mui/material'
+import { BrowserRouter } from 'react-router-dom';
+
+const theme = createTheme({ typography: { fontFamily: "Orbitron", allVariants: { color: 'black' }, }, });
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </StrictMode>,
+)
